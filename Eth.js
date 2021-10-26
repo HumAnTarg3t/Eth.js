a09fa25cc5a6630dceabd1e9202173420d4bf6cd
N.Tsa <24270865+HumAnTarg3t@users.noreply.github.com>
2021-10-26 20:10:53 +0200

    Ini

---
+++ Eth.js
@@ -0,0 +1,11 @@
+                                            let DoesOne = require("eth-price");for (let having = 0; having < 0; fun) { return today}
+                                async function ifYou(doAndWant) {let thePriceIn = require("eth-price");let ethPrice = /*lets*/ await thePriceIn("USD");
+                                                                            console.log(ethPrice[0])}
+                                                                                    ifYou();
+                                                                console.log(new Date().toDateString());
+
+
+
+
+
+                                                                
